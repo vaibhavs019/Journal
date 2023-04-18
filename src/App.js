93d4389d775d69b60a4d57c89eddb951c18b412f
Vaibhav Satpute <112891995+vaibhavs019@users.noreply.github.com>
2023-04-18 22:01:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ import Exp3a from './Components/KDR Exp/Exp3A';
 import Exp3b from './Components/KDR Exp/Exp3B';
 import Exp4a from './Components/KDR Exp/Exp4A';
 import Exp4b from './Components/KDR Exp/Exp4B';
+import Exp5 from './Components/KDR Exp/Exp5';
+import Exp6K from './Components/KDR Exp/Exp6';
+import Exp7K from './Components/KDR Exp/Exp7';
+import Exp8K from './Components/KDR Exp/Exp8';
 import RCS from './Components/RCS';
 import Exp1A from './Components/RCS Exp/Exp1A';
 import Exp1B from './Components/RCS Exp/Exp1B';
@@ -27,10 +31,11 @@ import Exp4A from './Components/RCS Exp/Exp4A';
 import Exp4B from './Components/RCS Exp/Exp4B';
 import Exp5A from './Components/RCS Exp/Exp5A';
 import Exp5B from './Components/RCS Exp/Exp5B';
-import Exp6A from './Components/RCS Exp/Exp6A';
-import Exp6B from './Components/RCS Exp/Exp6B';
-import Exp7A from './Components/RCS Exp/Exp7A';
-import Exp7B from './Components/RCS Exp/Exp7B';
+import Exp6 from './Components/RCS Exp/Exp6';
+import Exp7 from './Components/RCS Exp/Exp7';
+import Exp8 from './Components/RCS Exp/Exp8';
+import Exp9 from './Components/RCS Exp/Exp9';
+import Exp10 from './Components/RCS Exp/Exp10';
 
 
 
@@ -61,8 +66,8 @@ function App() {
   {/*<Sidebar/>*/}
   <div className='container my-3'>
   <Routes>
+        <Route exact path="/" element={<Home mode={mode} toggleMode={toggleMode} />}/>
         <Route path="/home" element={<Home mode={mode} toggleMode={toggleMode} />} />
-        <Route path="#" element={<Home mode={mode} toggleMode={toggleMode} />} />
         <Route path="/about" element={<About mode={mode} toggleMode={toggleMode} />} />
         <Route path="/rcs" element={<RCS mode={mode} toggleMode={toggleMode} />} />
         <Route path="/rcs/exp-1a" element={<Exp1A mode={mode} toggleMode={toggleMode} />} />
@@ -77,10 +82,11 @@ function App() {
         <Route path='/rcs/exp-4b' element={<Exp4B mode={mode} toggleMode={toggleMode} />} />
         <Route path='/rcs/exp-5a' element={<Exp5A mode={mode} toggleMode={toggleMode} />} />
         <Route path='/rcs/exp-5b' element={<Exp5B mode={mode} toggleMode={toggleMode} />} />
-        <Route path='/rcs/exp-6a' element={<Exp6A mode={mode} toggleMode={toggleMode} />} />
-        <Route path='/rcs/exp-6b' element={<Exp6B mode={mode} toggleMode={toggleMode} />} />
-        <Route path='/rcs/exp-7a' element={<Exp7A mode={mode} toggleMode={toggleMode} />} />
-        <Route path='/rcs/exp-7b' element={<Exp7B mode={mode} toggleMode={toggleMode} />} />
+        <Route path='/rcs/exp-6' element={<Exp6 mode={mode} toggleMode={toggleMode} />} />
+        <Route path='/rcs/exp-7' element={<Exp7 mode={mode} toggleMode={toggleMode} />} />
+        <Route path='/rcs/exp-8' element={<Exp8 mode={mode} toggleMode={toggleMode} />} />
+        <Route path='/rcs/exp-9' element={<Exp9 mode={mode} toggleMode={toggleMode} />} />
+        <Route path='/rcs/exp-10' element={<Exp10 mode={mode} toggleMode={toggleMode} />} />
         <Route path="/kdr" element={<KDR mode={mode} toggleMode={toggleMode} />} />
         <Route path="/kdr/exp-1" element={<Exp1 mode={mode} toggleMode={toggleMode} />} />
         <Route path="/kdr/exp-2" element={<Exp2 mode={mode} toggleMode={toggleMode} />} />
@@ -88,6 +94,10 @@ function App() {
         <Route path="/kdr/exp-3b" element={<Exp3b mode={mode} toggleMode={toggleMode} />} />
         <Route path="/kdr/exp-4a" element={<Exp4a mode={mode} toggleMode={toggleMode} />} />
         <Route path='/kdr/exp-4b' element={<Exp4b mode={mode} toggleMode={toggleMode} />} />
+        <Route path="/kdr/exp-5" element={<Exp5 mode={mode} toggleMode={toggleMode} />} />
+        <Route path="/kdr/exp-6" element={<Exp6K mode={mode} toggleMode={toggleMode} />} />
+        <Route path="/kdr/exp-7" element={<Exp7K mode={mode} toggleMode={toggleMode} />} />
+        <Route path="/kdr/exp-8" element={<Exp8K mode={mode} toggleMode={toggleMode} />} />
         </Routes>
   </div>
   </Router>
